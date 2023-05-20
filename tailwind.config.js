b94e93e7975351629c98fc08ceff330bf06fcee9
Okwudili Joshua Odumodu <okwyodumodu@gmail.com>
2023-05-20 18:57:36 +0100

    More tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,43 @@
+import { pxToRem } from "./src/components/utils/pxToRem";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
+    fontSize: {
+      xs: pxToRem(12),
+      sm: pxToRem(14),
+      base: pxToRem(16),
+      lg: pxToRem(18),
+      xl: pxToRem(20),
+      "2xl": pxToRem(24),
+      "3xl": pxToRem(30),
+      "4xl": pxToRem(36),
+      "5xl": pxToRem(40),
+      "6xl": pxToRem(48),
+    },
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
     colors: {
-      dark: {
-        900: "#000000",
-        // 400: ' #333333',
-        // 300: '#C4C4C4',
-        // 200: '#787878',
-        // 100: '#C4C4C4',
-        // 50: '#E8E8E8',
-        // 10: '#F7F7F7',
-      },
-      blue: "#0149B5",
-      white: "#FFFDF9",
-      accent: "#0149B5",
-      grey: "#646464",
-      black: "#1B1C1E",
       transparent: "transparent",
+      white: "#FFFFFF",
+      light: "#FFFDF9",
+      black: "#000000",
+      dark: "#1B1C1E",
+      grey: "#646464",
+      accent: "#0149B5",
+      blue: "#0149B5",
+      red: "#FF576B",
     },
     screens: {
       sm: "480px",
