706b120d4af09fbd11facbe564658dd6d408e650
Okwudili Joshua Odumodu <okwyodumodu@gmail.com>
2023-05-21 02:53:00 +0100

    Make CustomButton reusable

--- src/components/atoms/CustomButton/CustomButton.tsx
+++ src/components/atoms/CustomButton/CustomButton.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 interface CustomButtonProps {
   onClick?: () => void;
   disabled?: boolean;
   children: React.ReactNode;
+  className?: string;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   onClick,
   disabled = false,
   children,
+  className,
 }) => {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className={`${className} bg-light hover:bg-red text-blue hover:text-light text-2xl font-semibold py-[16px] px-[40px] rounded-[36px]`}
       onClick={onClick}
       disabled={disabled}
     >
