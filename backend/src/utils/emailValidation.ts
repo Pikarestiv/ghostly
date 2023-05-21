97381afebed5c0a1216bbde30d904581869bc6c8
Okwudili Joshua Odumodu <okwyodumodu@gmail.com>
2023-05-21 02:54:09 +0100

    Add emailValiidation util

--- backend/src/utils/emailValidation.ts
+++ backend/src/utils/emailValidation.ts
@@ -1,10 +1,8 @@
 // validation.js
-const validateEmail = (email: string) => {
+export const validateEmail = (email: string) => {
   // Regular expression for email validation
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
   // Validate email format
   return emailRegex.test(email);
 };
-
-module.exports = { validateEmail };
